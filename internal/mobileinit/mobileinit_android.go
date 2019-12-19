93dd3c36e9136f3ea8572b0561b3f6b0b2aaa8d4
Steeve Morin <steeve@zen.ly>
2019-12-19 16:29:48 +0100

    internal/mobileinit: remove stderr redirection
    The stderr redirection will trigger an ANR in case of a panic, because
    the Go runtime will dump its content directly on stderr, which is not
    read anymore by the goroutine. Remove that code completely.
    
    Signed-off-by: Steeve Morin <steeve@zen.ly>

--- internal/mobileinit/mobileinit_android.go
+++ internal/mobileinit/mobileinit_android.go
@@ -29,7 +29,6 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"syscall"
 	"unsafe"
 )
 
@@ -71,24 +70,4 @@ func init() {
 	log.SetOutput(infoWriter{})
 	// android logcat includes all of log.LstdFlags
 	log.SetFlags(log.Flags() &^ log.LstdFlags)
-
-	r, w, err := os.Pipe()
-	if err != nil {
-		panic(err)
-	}
-	stderr = w
-	if err := syscall.Dup3(int(w.Fd()), int(os.Stderr.Fd()), 0); err != nil {
-		panic(err)
-	}
-	go lineLog(r, C.ANDROID_LOG_ERROR)
-
-	r, w, err = os.Pipe()
-	if err != nil {
-		panic(err)
-	}
-	stdout = w
-	if err := syscall.Dup3(int(w.Fd()), int(os.Stdout.Fd()), 0); err != nil {
-		panic(err)
-	}
-	go lineLog(r, C.ANDROID_LOG_INFO)
 }
